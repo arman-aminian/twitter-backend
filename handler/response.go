5afd5ddd9fa5ddd6ac1e5a2fd2e51f154d32f7c2
Arman Aminian <arman.aminian78@gmail.com>
2021-01-28 10:54:59 +0330

    like/retweet list fixed : ech result has is_following

--- handler/response.go
+++ handler/response.go
@@ -264,10 +264,20 @@ type tweetLikeAndRetweetResponse struct {
 	RetweetsList *[]model.Owner `json:"retweets" bson:"retweets"`
 }
 
-func newLikeAndRetweetResponse(t *model.Tweet) *tweetLikeAndRetweetResponse {
+func newLikeAndRetweetResponse(us user.Store, srcUsername string, t *model.Tweet) *tweetLikeAndRetweetResponse {
 	tr := new(tweetLikeAndRetweetResponse)
-	tr.LikesList = t.Likes
-	tr.RetweetsList = t.Retweets
+
+	temp := *t.Likes
+	for i := range temp {
+		temp[i].IsFollowing, _ = us.IsFollower(temp[i].Username, srcUsername)
+	}
+	tr.LikesList = &temp
+
+	temp2 := *t.Retweets
+	for i := range temp2 {
+		temp2[i].IsFollowing, _ = us.IsFollower(temp2[i].Username, srcUsername)
+	}
+	tr.RetweetsList = &temp2
 	return tr
 }
 
