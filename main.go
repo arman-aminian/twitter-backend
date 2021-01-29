f81a36767fc0ebc5aa10a7d1ce0ab363f1420026
Arman Aminian <arman.aminian78@gmail.com>
2021-01-29 18:58:53 +0330

    Timeline updated.

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 		// RUN THIS IF YOUR HASHTAG DATABASE IS EMPTY
 		// StartUpTrends(ts, h)
 
-		r.Logger.Fatal(r.Start(":" + port))
+		r.Logger.Fatal(r.Start("0.0.0.0:" + port))
 	}
 
 	// if testHeap {
--- store/tweet.go
+++ store/tweet.go
@@ -7,7 +7,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"regexp"
-	"time"
 )
 
 type TweetStore struct {
@@ -150,12 +149,12 @@ func (ts *TweetStore) ExtractHashtags(t *model.Tweet) map[string]int {
 }
 
 func (ts *TweetStore) GetTimelineFromTweetIDs(tweetsIDs []primitive.ObjectID, day int) (*[]model.Tweet, error) {
-	date := time.Now().AddDate(0, 0, day).Format("2006-01-02")
+	//date := time.Now().AddDate(0, 0, day).Format("2006-01-02")
 	var tweets []model.Tweet
 	filter := bson.M{
 		"$and": []bson.M{
 			{"_id": bson.M{"$in": tweetsIDs}},
-			{"date": date},
+			//{"date": date},
 		},
 	}
 
